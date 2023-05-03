1f350ab866df453d28d8e33d05ffb6e664463435
Nikita Mauvisseau <nikita.mauvisseau@cosmotech.com>
2023-05-03 10:02:14 +0200

    refactor: [PROD-11086] Remove tab customizations

--- src/utils/scenarioParameters/custom/ParametersGroupsComponentsMapping.js
+++ src/utils/scenarioParameters/custom/ParametersGroupsComponentsMapping.js
@@ -1,22 +1,22 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
-import {
-  FlowManagementPoliciesParameterTab,
-  SimulationParameterTab,
-  MassActionLeverParameterTab,
-  ModelBehaviorParameterTab,
-  OptimizationParameterTab,
-  UncertaintyAnalysisParameterTab,
-  SensitivityAnalysisParameterTab,
-} from '../factories';
+// import {
+//   FlowManagementPoliciesParameterTab,
+//   SimulationParameterTab,
+//   MassActionLeverParameterTab,
+//   ModelBehaviorParameterTab,
+//   OptimizationParameterTab,
+//   UncertaintyAnalysisParameterTab,
+//   SensitivityAnalysisParameterTab,
+// } from '../factories';
 
 // You can define here the tab components mapping for the custom parameters group of your solution
 export const CUSTOM_PARAMETERS_GROUPS_COMPONENTS_MAPPING = {
-  simulation: SimulationParameterTab,
-  mass_action_lever: MassActionLeverParameterTab,
-  optimization: OptimizationParameterTab,
-  model_behavior: ModelBehaviorParameterTab,
-  flow_management_policies: FlowManagementPoliciesParameterTab,
-  uncertainty_analysis: UncertaintyAnalysisParameterTab,
-  sensitivity_analysis: SensitivityAnalysisParameterTab,
+  // simulation: SimulationParameterTab,
+  // mass_action_lever: MassActionLeverParameterTab,
+  // optimization: OptimizationParameterTab,
+  // model_behavior: ModelBehaviorParameterTab,
+  // flow_management_policies: FlowManagementPoliciesParameterTab,
+  // uncertainty_analysis: UncertaintyAnalysisParameterTab,
+  // sensitivity_analysis: SensitivityAnalysisParameterTab,
 };
--- src/utils/scenarioParameters/factories/index.js
+++ src/utils/scenarioParameters/factories/index.js
@@ -1,17 +1,17 @@
-import { FlowManagementPoliciesParameterTab } from './FlowManagementPoliciesParameterTab';
-import { MassActionLeverParameterTab } from './MassActionLeverParameterTab';
-import { ModelBehaviorParameterTab } from './ModelBehaviorParameterTab';
-import { OptimizationParameterTab } from './OptimizationParameterTab';
-import { SensitivityAnalysisParameterTab } from './SensitivityAnalysisParameterTab';
-import { SimulationParameterTab } from './SimulationParameterTab';
-import { UncertaintyAnalysisParameterTab } from './UncertaintyAnalysisParameterTab';
+// import { FlowManagementPoliciesParameterTab } from './FlowManagementPoliciesParameterTab';
+// import { default as MassActionLeverParameterTab } from './MassActionLeverParameterTab';
+// import { ModelBehaviorParameterTab } from './ModelBehaviorParameterTab';
+// import { OptimizationParameterTab } from './OptimizationParameterTab';
+// import { SensitivityAnalysisParameterTab } from './SensitivityAnalysisParameterTab';
+// import { SimulationParameterTab } from './SimulationParameterTab';
+// import { UncertaintyAnalysisParameterTab } from './UncertaintyAnalysisParameterTab';
 
-export {
-  FlowManagementPoliciesParameterTab,
-  MassActionLeverParameterTab,
-  ModelBehaviorParameterTab,
-  OptimizationParameterTab,
-  SensitivityAnalysisParameterTab,
-  SimulationParameterTab,
-  UncertaintyAnalysisParameterTab,
-};
+// export {
+//   FlowManagementPoliciesParameterTab,
+//   MassActionLeverParameterTab,
+//   ModelBehaviorParameterTab,
+//   OptimizationParameterTab,
+//   SensitivityAnalysisParameterTab,
+//   SimulationParameterTab,
+//   UncertaintyAnalysisParameterTab,
+// };
